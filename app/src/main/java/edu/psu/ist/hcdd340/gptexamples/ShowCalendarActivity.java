8e945513c83b4ff3e6a30c250115a6cddb6adb69
Saeed Abdullah <me@saeedabdullah.com>
2023-11-15 21:45:53 -0500

    calendar view starter files

---
+++ app/src/main/java/edu/psu/ist/hcdd340/gptexamples/ShowCalendarActivity.java
@@ -0,0 +1,14 @@
+package edu.psu.ist.hcdd340.gptexamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_calendar);
+    }
+}
